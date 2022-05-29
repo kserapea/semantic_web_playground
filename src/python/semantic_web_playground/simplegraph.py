b643741abb05448ca430d7b63a1b98734eba8523
Sarah Kuehl <kuehlse@gmail.com>
2022-05-29 09:57:37 -0400

    initial project setup

---
+++ src/python/semantic_web_playground/simplegraph.py
@@ -0,0 +1,7 @@
+
+
+class SimpleGraph:
+    def __init__(self):
+        self._spo = {}
+        self._pos = {}
+        self._osp = {}
---
+++ src/python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="semantic_web_playground",
+    version="0.0.1",
+    url="https://github.com/kserapea/semantic_web_playground",
+    packages=["semantic_web_playground"],
+    zip_safe=False
+)
